3f262d068624def4b7c4b7bae9dd42d6502d15cb
TheSeeker427 <carr427b@gmail.com>
2016-11-17 11:27:23 -0500

    init

---
+++ Eevee/IPAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eevee
+{
+    class IPAddress
+    {
+        String ip;
+        String loc;
+        String hostname;
+        String city;
+        String region;
+        String country;
+        String org;
+        String postal;
+    }
+}
